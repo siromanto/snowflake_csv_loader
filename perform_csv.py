de088a81731b325c9e3b327b18ff09be58135a87
siromanto <dmytrenko.serhii@gmail.com>
2019-02-08 13:00:51 +0200

    clear comments

--- perform_csv.py
+++ perform_csv.py
@@ -2,22 +2,13 @@
 
 import csv
 import os
-import pandas as pd
-import datetime
 
 from configs import config, helpers
 
 
 class PerformCsv:
-    # for filename in os.listdir(os.getcwd()):
-
-    # def prepare_header_for_new_csv(self, file):
-    #     writer = csv.DictWriter(file, fieldnames=FIELDNAMES)
-    #     writer.writeheader()
-    #     return writer
     def __init__(self, config):
         self.config = config
-        # self.conn = self.create_connection()
 
     def create_connection(self):
         conn = helpers.establish_db_conn(
@@ -28,11 +19,6 @@ class PerformCsv:
             self.config.WAREHOUSE
         )
         return conn
-
-
-    def investigate_csv(self, filename):
-        pass
-
 
     def parse_columns_names(self, headers):
         return [n.replace('(', '').replace(')', '').replace(" ", "_").replace("-", "_").upper() for n in headers]
@@ -56,42 +42,20 @@ class PerformCsv:
 
                     for raw_row in reader:
                         row = dict(zip(clear_headers, raw_row))
-
-                        # self.validate_fields(row)
-
                         writer.writerow(row)
 
                     print(f'New new file creating ...')
-
 
     def prepare_header_for_new_csv(self, file, headers):
         writer = csv.DictWriter(file, fieldnames=headers)
         writer.writeheader()
         return writer
 
-    # def validate_fields(self, row):
-    #     row['START_DATE_IN_UTC'] = self.transform_date_to_correct_format(row['START_DATE_IN_UTC'])
-    #     row['CAMPAIGN_GROUP_START_DATE'] = self.transform_date_to_correct_format(row['CAMPAIGN_GROUP_START_DATE'])
-    #
-    # def transform_date_to_correct_format(self, date_str):
-    #     datetime_obj = datetime.datetime.strptime(date_str, helpers.DATETIME_FORMAT)
-    #     return str(datetime_obj.date())
-
-
     def run(self, report_folder):
         self.transform_csvs_to_correct_format(report_folder)
 
 
-
-
-
-
-
-
 if __name__ == '__main__':
-    # filename = 'account_507488250_campaign_performance_report_1.1.2017-6.30.2017.csv'
-    filename = 'test_clear.csv'
-
     PerformCsv(config).run(report_folder='campaign_performance')
     # PerformCsv(config).run(report_folder='ad_performance')
 
